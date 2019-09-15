5669c3b9d85dd55e6c69dfd08937971ba1d63df7
Laird Sapir <laird@memphismckay.com>
2019-09-15 00:28:47 -0400

    removed mbt-button module

--- blocks/src/init.php
+++ blocks/src/init.php
@@ -6,5 +6,4 @@
 require_once plugin_dir_path( __FILE__ ) . 'mbt-book/index.php';
 require_once plugin_dir_path( __FILE__ ) . 'author-profile/index.php';
 require_once plugin_dir_path( __FILE__ ) . 'social-link/index.php';
-require_once plugin_dir_path( __FILE__ ) . 'mbt-buylinks/index.php';
 require_once plugin_dir_path( __FILE__ ) . 'recent-posts/index.php';
