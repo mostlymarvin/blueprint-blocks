046d1e7b79df9688a1bfdfe5c57386ce5aa86324
Laird Sapir <laird@memphismckay.com>
2019-09-16 18:52:27 -0400

    bugfix: extra '/' in path to button packs

--- inc/button-packs.php
+++ inc/button-packs.php
@@ -11,7 +11,7 @@
 function blueprint_extras_add_button_packs( $folders ) {
 
     $styledir = BLUEPRINT_BLOCKS_PLUGIN_PATH . 'assets/button-packs/';
-    $styleuri =  plugins_url() . '/blueprint-blocks/assets/button-packs/';
+    $styleuri =  plugins_url() . '/blueprint-blocks/assets/button-packs';
     $folders[] = array(
         'dir' => $styledir,
         'url' => $styleuri,
